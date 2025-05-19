7ecce1fae4fddadb2a87804a571c98d9fdcbfd3f
haury <haury@cip.ifi.lmu.de>
2025-05-19 11:10:21 +0200

    added walking and idle animation to elliot object / reorganized folder structure

--- Assets/_GAME_/Player/Skripts/PlayerMovement.cs
+++ Assets/_GAME_/Player/Skripts/PlayerMovement.cs
@@ -5,12 +5,19 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     public float moveSpeed = 5f;
+
     private Rigidbody2D rb;
     private Vector2 movement;
+
+    private Animator animator;
+    private SpriteRenderer spriteRenderer;
 
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+
     }
 
     void Update()
@@ -19,6 +26,16 @@ public class PlayerMovement : MonoBehaviour
         movement.x = Input.GetAxisRaw("Horizontal");
         movement.y = Input.GetAxisRaw("Vertical");
         movement.Normalize(); // Prevent faster diagonal movement
+
+        // Animation setzen
+        bool isMoving = movement.x != 0 || movement.y != 0;
+        animator.SetBool("isWalking", isMoving);
+
+        // Sprite spiegeln (nur bei horizontaler Bewegung)
+        if (movement.x > 0)
+            spriteRenderer.flipX = false;
+        else if (movement.x < 0)
+            spriteRenderer.flipX = true;
     }
 
     void FixedUpdate()
